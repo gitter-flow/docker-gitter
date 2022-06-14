3f1f7075b2d0d189f89df1dc6638b5de0937f98f
paulb <barrie.paul@protonmail.com>
2022-06-14 22:23:16 +0200

    feat: add traefik

---
+++ token.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+curl \
+  -d "client_id=api-social" \
+  -d "client_secret=h8lCO4plzKFfsong2crbHl7y1fhCykpl" \
+  -d "grant_type=password" \
+  -d "username=paulc" \
+  -d "password=password" \
+  -d "scope=openid" \
+  "http://gitter.localhost/auth/realms/gitter/protocol/openid-connect/token"
