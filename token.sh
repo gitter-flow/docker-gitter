a0cdbac277a5433f4627022389ac0a44a791f2c9
Fred <frederic.Flaceliere@gmail.com>
2022-06-20 18:52:39 +0200

    update compose

--- token.sh
+++ token.sh
@@ -2,9 +2,9 @@
 
 curl -s \
   -d "client_id=api-social" \
-  -d "client_secret=h8lCO4plzKFfsong2crbHl7y1fhCykpl" \
+  -d "client_secret=AzoICF0dvK8pfsJFto7Qso1AtA8ExWuB" \
   -d "grant_type=password" \
-  -d "username=pbarrie" \
+  -d "username=f" \
   -d "password=password" \
   -d "scope=openid" \
   "http://gitter.localhost/auth/realms/gitter/protocol/openid-connect/token" | jq .access_token
